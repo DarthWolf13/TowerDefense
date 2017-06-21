b58bb1fc086bba089de358dbef30ccff62cde4b2
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-06-21 19:44:45 +0200

    Step 2

--- TowerDefense/TowerDefense/PlayingState.cs
+++ TowerDefense/TowerDefense/PlayingState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,22 @@ namespace TowerDefense
     class PlayingState : GameObjectList
     {
         homeBase home;
+        GameObjectList ufos;
 
         public PlayingState()
         {
             home = new homeBase();
+            ufos = new GameObjectList();
 
             this.Add(new SpriteGameObject("spr_background"));
             this.Add(home);
+            this.Add(ufos);
+
+            this.ufos.Add(new Ufo(new Vector2(20, 20), home));
+            this.ufos.Add(new Ufo(new Vector2(-100, 600), home));
+            this.ufos.Add(new Ufo(new Vector2(-300, 300), home));
+            this.ufos.Add(new Ufo(new Vector2(800, 500), home));
+            this.ufos.Add(new Ufo(new Vector2(700, 1000), home));
         }
     }
 }
---
+++ TowerDefense/TowerDefense/Ufo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerDefense
+{
+    class Ufo : RotatingSpriteGameObject
+    {
+        public Ufo(Vector2 startPosition, GameObject targetObject) : base("spr_ufo")
+        {
+            this.origin = sprite.Center;
+            this.position = startPosition;
+            LookAt(targetObject);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            this.velocity = AngularDirection * 50;
+        }
+    }
+}
